4f390ea1f871836d67fb9bd06bee9b7c14048f54
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-11-05 02:06:05 -0400

    Work in progress.

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -5,22 +5,88 @@ package rocks.zipcodewilmington.tictactoe;
  */
 public class Board {
     public Board(Character[][] matrix) {
+        matrix = new Character[][]{{'X', 'O', 'X'}, {'O', 'X', 'O'}, {'X', 'O', 'X'}};
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        boolean isWinner = false;
+        Character[][] matrix = new Character[][]{{'X', 'O', 'X'}, {'O', 'X', 'O'}, {'X', 'O', 'X'}};
+
+        if ((matrix[0][0].toString() + matrix[0][1].toString() + matrix[0][2].toString()).equals("XXX")) {
+            isWinner = true;
+        } else if ((matrix[1][0].toString() + matrix[1][1].toString() + matrix[1][2].toString()).equals("XXX")) {
+            isWinner = true;
+        } else if ((matrix[2][0].toString() + matrix[2][1].toString() + matrix[2][2].toString()).equals("XXX")) {
+            isWinner = true;
+        }
+
+        if ((matrix[0][0].toString() + matrix[1][0].toString() + matrix[2][0].toString()).equals("XXX")) {
+            isWinner = true;
+        } else if ((matrix[0][1].toString() + matrix[1][1].toString() + matrix[2][1].toString()).equals("XXX")) {
+            isWinner = true;
+        } else if ((matrix[0][2].toString() + matrix[1][2].toString() + matrix[2][2].toString()).equals("XXX")) {
+            isWinner = true;
+        }
+
+        if ((matrix[0][0].toString() + matrix[1][1].toString() + matrix[2][2].toString()).equals("XXX")) {
+            isWinner = true;
+        } else if ((matrix[2][0].toString() + matrix[1][1].toString() + matrix[0][2].toString()).equals("XXX")) {
+            isWinner = true;
+        }
+
+        return isWinner;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        boolean isWinner = false;
+        Character[][] matrix = new Character[][]{{'X', 'O', 'X'}, {'O', 'X', 'O'}, {'X', 'O', 'X'}};
+
+        if ((matrix[0][0].toString() + matrix[0][1].toString() + matrix[0][2].toString()).equals("OOO")) {
+            isWinner = true;
+        } else if ((matrix[1][0].toString() + matrix[1][1].toString() + matrix[1][2].toString()).equals("OOO")) {
+            isWinner = true;
+        } else if ((matrix[2][0].toString() + matrix[2][1].toString() + matrix[2][2].toString()).equals("OOO")) {
+            isWinner = true;
+        }
+
+        if ((matrix[0][0].toString() + matrix[1][0].toString() + matrix[2][0].toString()).equals("OOO")) {
+            isWinner = true;
+        } else if ((matrix[0][1].toString() + matrix[1][1].toString() + matrix[2][1].toString()).equals("OOO")) {
+            isWinner = true;
+        } else if ((matrix[0][2].toString() + matrix[1][2].toString() + matrix[2][2].toString()).equals("OOO")) {
+            isWinner = true;
+        }
+
+        if ((matrix[0][0].toString() + matrix[1][1].toString() + matrix[2][2].toString()).equals("OOO")) {
+            isWinner = true;
+        } else if ((matrix[2][0].toString() + matrix[1][1].toString() + matrix[0][2].toString()).equals("OOO")) {
+            isWinner = true;
+        }
+
+        return isWinner;
     }
 
     public Boolean isTie() {
-        return null;
+        boolean tie = true;
+
+        if (isInFavorOfX()) {
+            tie = false;
+        } else if (isInFavorOfO()) {
+            tie = false;
+        }
+
+        return tie;
     }
 
     public String getWinner() {
-        return null;
-    }
+        String winner = "";
 
+        if (isInFavorOfX()) {
+            winner = "X";
+        } else if (isInFavorOfO()) {
+            winner = "O";
+        }
+
+        return winner;
+    }
 }
